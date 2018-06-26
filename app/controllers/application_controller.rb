9ff929d3022d0e57fb817191edfb44cba6f73dd8
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-06-26 18:30:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,6 +9,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    redirect '/posts'
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts/:id' do
+    post_id = params[:id]
+    @post = Post.find(post_id)
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    post_id = params[:id]
+    @post = Post.find(post_id)
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do
+    post_id = params[:id]
+    post = Post.find(post_id)
+    post.destroy
+    erb :delete
+  end
+
+  patch '/posts/:id' do
+    post_id = params[:id]
+    post = Post.find(post_id)
+    post.update(name: params[:name], content: params[:content])
+    redirect "/posts/#{post_id}"
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    #binding.pry
+    erb :index
+  end
+
+  post '/posts' do
+    #use params[] to create a new blog post with ActiveRecord
+    Post.create(params)
+    redirect "/posts"
+  end
+
 end
